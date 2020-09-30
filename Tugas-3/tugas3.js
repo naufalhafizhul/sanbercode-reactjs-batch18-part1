64b005bd7cfb43ef9f06820edeb4dd2dea3bd91a
naufalhafizhul <naufallvi48@gmail.com>
2020-09-30 17:52:57 +0700

    memperbaiki soal 4

--- Tugas-3/tugas3.js
+++ Tugas-3/tugas3.js
@@ -41,19 +41,20 @@ console.log('Kata Keempat: ' + kataKeempat);
 console.log('Kata Kelima: ' + kataKelima);
 
 // soal 4
-var nilai = 80 ;
-if (nilai >= 80) {
-    console.log ('Nilai A')
-} else if (nilai >= 70, nilai < 80){
-    console.log ('Nilai B')
-} else if (nilai >= 60, nilai < 70){
-    console.log ('Nilai C')
-} else if (nilai >= 50, nilai < 60){
-    console.log ('Nilai D')
-} else {
-    console.log('Nilai E')
-}
+var nilai = 55
 
+if ( nilai >= 80 ) {
+    console.log("Nilai A")
+    }
+      else if(nilai >= 70 && nilai < 80 ) {
+        console.log("Nilai B")    
+    } else if( nilai >= 60 && nilai < 70) {
+        console.log("Nilai C")
+    } else if ( nilai >= 50 && nilai <60 ) {
+        console.log("Nilai D")
+    } else {
+        console.log("Nilai E")
+    }
 
 
 // soal 5 
